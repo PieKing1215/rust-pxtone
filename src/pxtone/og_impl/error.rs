ccc54591fbee63343da579b2b14e9e6ca7de4733
David M <PieKing1215@users.noreply.github.com>
2022-09-08 00:26:42 -0400

    Replace num-traits/num-derive with manual to/from

--- src/pxtone/og_impl/error.rs
+++ src/pxtone/og_impl/error.rs
@@ -1,11 +1,9 @@
 use core::fmt;
 use std::ffi::CStr;
 
-use num_derive::{FromPrimitive, ToPrimitive};
-use num_traits::{FromPrimitive, ToPrimitive};
 use pxtone_sys::{pxtnERR, pxtnError_get_string};
 
-#[derive(Debug, Copy, Clone, FromPrimitive, ToPrimitive)]
+#[derive(Debug, Copy, Clone)]
 pub enum Error {
     VOID = 1,
     INIT = 2,
@@ -34,10 +32,46 @@ pub enum Error {
     X1XIgnore = 25,
     X3XAddTuning = 26,
     X3XKey = 27,
-    Num = 28,
 }
 
 impl Error {
+    fn from_i32(n: i32) -> Option<Self> {
+        match n {
+            1 => Some(Self::VOID),
+            2 => Some(Self::INIT),
+            3 => Some(Self::FATAL),
+            4 => Some(Self::AntiOpreation),
+            5 => Some(Self::DenyBeatclock),
+            6 => Some(Self::DescW),
+            7 => Some(Self::DescR),
+            8 => Some(Self::DescBroken),
+            9 => Some(Self::FmtNew),
+            10 => Some(Self::FmtUnknown),
+            11 => Some(Self::InvCode),
+            12 => Some(Self::InvData),
+            13 => Some(Self::Memory),
+            14 => Some(Self::MooInit),
+            15 => Some(Self::Ogg),
+            16 => Some(Self::OggNoSupported),
+            17 => Some(Self::Param),
+            18 => Some(Self::PcmConvert),
+            19 => Some(Self::PcmUnknown),
+            20 => Some(Self::PtnBuild),
+            21 => Some(Self::PtnInit),
+            22 => Some(Self::PtvNoSupported),
+            23 => Some(Self::TooMuchEvent),
+            24 => Some(Self::WoiceFull),
+            25 => Some(Self::X1XIgnore),
+            26 => Some(Self::X3XAddTuning),
+            27 => Some(Self::X3XKey),
+            _ => None,
+        }
+    }
+
+    fn to_i32(self) -> i32 {
+        self as _
+    }
+
     pub fn from_raw(value: pxtnERR) -> Result<(), Error> {
         Self::from_i32(value as i32).map_or(Ok(()), Err)
     }
@@ -46,11 +80,9 @@ impl Error {
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         unsafe {
-            let msg = self.to_i32().map_or("???", |err| {
-                CStr::from_ptr(pxtnError_get_string(err as pxtnERR))
-                    .to_str()
-                    .unwrap()
-            });
+            let msg = CStr::from_ptr(pxtnError_get_string(self.to_i32() as pxtnERR))
+                .to_str()
+                .unwrap();
             write!(f, "{}", msg)
         }
     }
